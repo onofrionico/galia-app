d1e1a5918e90cbdfc10c8da3c2ac341b93cf580b
onofrionico <onofrionico@gmail.com>
2026-02-21 12:53:28 -0300

    Add empty migration file for remaining expense columns
    - Create migration stub with revision ID 'add_remaining_expense_columns'
    - Set down_revision to 'update_expenses_model'
    - Include empty upgrade/downgrade functions as placeholder

---
+++ backend/migrations/versions/add_remaining_expense_columns.py
@@ -0,0 +1,24 @@
+"""add remaining expense columns
+
+Revision ID: add_remaining_expense_columns
+Revises: update_expenses_model
+Create Date: 2026-02-21 12:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'add_remaining_expense_columns'
+down_revision = 'update_expenses_model'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
